ccdbf7376c1339b31695d2e31ed6ee93ea48b6b2
Isak Horvath <48546805+tavro@users.noreply.github.com>
2023-05-21 19:32:18 +0200

    NEW: generate parse tree

--- src/parser.ts
+++ src/parser.ts
@@ -5,11 +5,45 @@ interface HTMLNode {
 }
 
 const parse = (input: string): HTMLNode => {
-  const stack: HTMLNode[] = [];
+  const lines = input.trim().split('\n');
 
-  // TODO: implement parser
+  const parseNode = (indentation: number): HTMLNode => {
+    const line = lines.shift() || '';
+    const node: HTMLNode = {
+      tag: '',
+      children: [],
+      text: undefined
+    };
 
-  return stack[0];
+    const trimmedLine = line.trim();
+    const lineIndentation = line.length - trimmedLine.length;
+
+    if (lineIndentation === indentation) {
+      if (trimmedLine.includes(' ')) {
+        const [tag, text] = trimmedLine.split(' ', 2);
+        node.tag = tag;
+        node.text = text;
+      } else {
+        node.tag = trimmedLine;
+      }
+
+      while (lines.length > 0) {
+        const nextLine = lines[0];
+        const nextLineIndentation = nextLine.length - nextLine.trimStart().length;
+
+        if (nextLineIndentation > lineIndentation) {
+          const childNode = parseNode(lineIndentation + 2);
+          node.children.push(childNode);
+        } else {
+          break;
+        }
+      }
+    }
+
+    return node;
+  };
+
+  return parseNode(0);
 };
 
 const template = `
@@ -21,4 +55,4 @@ html
 `;
 
 const parsedTemplate = parse(template);
-console.log(parsedTemplate);
+console.log(parsedTemplate.children);
