3a2bd2e5c7dbf9daecf161ea549156c9348bd082
Isak Horvath <isakhorvath@gmail.com>
2023-05-24 12:00:58 +0200

    NEW: Improve website design

--- vue-frontend/simp-frontend/src/views/AboutView.vue
+++ vue-frontend/simp-frontend/src/views/AboutView.vue
@@ -1,49 +1,58 @@
 <template>
   <div class="about">
     <div class="left">
+      <h2>input.simp</h2>
+      <hr/><br/>
       <p>
-        <span class="tag">html</span><br/>
-        &emsp;<span class="tag">head</span><br/>
-        &emsp;&emsp;<span class="tag">title</span> Test test test<br/>
-        &emsp;<span class="tag">body</span><br/>
-        &emsp;&emsp;<span class="tag">h1</span><span class="identifier">#testId</span> Test test<br/>
-        &emsp;&emsp;<span class="tag">img[</span>src=<span class="string">"./test.png"</span><span class="tag"> ]</span><br/>
-        &emsp;&emsp;<span class="tag">p</span> Test<br/>
-        &emsp;&emsp;&emsp;<span class="tag">div</span><span class="identifier">.testClass</span><br/>
-        &emsp;&emsp;&emsp;&emsp;<span class="tag">a[</span>href=<span class="string">"#"</span><span class="tag"> ]</span> Test test test test<br/>
-        &emsp;&emsp;&emsp;&emsp;<span class="tag">p</span> Test test test test test
+        <div class="number">1</div><span class="tag">html</span><br/>
+        <div class="number">2</div>&emsp;<span class="tag">head</span><br/>
+        <div class="number">3</div>&emsp;&emsp;<span class="tag">title</span> hello world!<br/>
+        <div class="number">4</div>&emsp;<span class="tag">body</span><br/>
+        <div class="number">5</div>&emsp;&emsp;<span class="tag">h1</span><span class="identifier">#headerId</span> hello world!<br/>
+        <div class="number">6</div>&emsp;&emsp;<span class="tag">img[</span>src=<span class="string">"./img.png"</span><span class="tag"> ]</span><br/>
+        <div class="number">7</div>&emsp;&emsp;<span class="tag">p</span> hello world!<br/>
+        <div class="number">8</div>&emsp;&emsp;<span class="tag">div</span><span class="identifier">.divClass</span><br/>
+        <div class="number">9</div>&emsp;&emsp;&emsp;<span class="tag">a[</span>href=<span class="string">"#"</span><span class="tag"> ]</span> click me!<br/>
+        <div class="number">10</div>&emsp;&emsp;&emsp;<span class="tag">p</span> SIMPing is fun!
       </p>
     </div>
     <div class="right">
+      <h2>output.html</h2>
+      <hr/><br/>
       <p>
-        &lt;html&gt;<br/>
-        &emsp;&lt;head&gt;<br/>
-        &emsp;&emsp;&lt;title&gt;Test test test&lt;/title&gt;<br/>
-        &emsp;&lt;/head&gt;<br/>
-        &emsp;&lt;body&gt;<br/>
-        &emsp;&emsp;&lt;h1 id="testId"&gt;Test test&lt;/h1&gt;<br/>
-        &emsp;&emsp;&lt;img src="./test.png"/&gt;<br/>
-        &emsp;&emsp;&lt;p&gt;Test<br/>
-        &emsp;&emsp;&emsp;&lt;div class="testClass"&gt;<br/>
-        &emsp;&emsp;&emsp;&emsp;&lt;a href="#"&gt;Test test test test&lt;/a&gt;<br/>
-        &emsp;&emsp;&emsp;&lt;/div&gt;<br/>
-        &emsp;&emsp;&lt;/p&gt;<br/>
-        &emsp;&emsp;&lt;p&gt;Test test test test test&lt;/p&gt;<br/>
-        &emsp;&lt;/body&gt;<br/>
-        &lt;/html&gt;
+        <div class="number">1</div>&lt;<span class="tag">html</span>&gt;<br/>
+        <div class="number">2</div>&emsp;&lt;<span class="tag">head</span>&gt;<br/>
+        <div class="number">3</div>&emsp;&emsp;&lt;<span class="tag">title</span>&gt;hello world!&lt;/<span class="tag">title</span>&gt;<br/>
+        <div class="number">4</div>&emsp;&lt;/<span class="tag">head</span>&gt;<br/>
+        <div class="number">5</div>&emsp;&lt;<span class="tag">body</span>&gt;<br/>
+        <div class="number">6</div>&emsp;&emsp;&lt;<span class="tag">h1</span> id=<span class="string">"headerId" </span>&gt;hello world!&lt;/<span class="tag">h1</span>&gt;<br/>
+        <div class="number">7</div>&emsp;&emsp;&lt;<span class="tag">img</span> src=<span class="string">"./img.png" </span>/&gt;<br/>
+        <div class="number">8</div>&emsp;&emsp;&lt;<span class="tag">p</span>&gt;hello world!&lt;/<span class="tag">p</span>&gt;<br/>
+        <div class="number">9</div>&emsp;&emsp;&lt;<span class="tag">div</span> class=<span class="string">"divClass" </span>&gt;<br/>
+        <div class="number">10</div>&emsp;&emsp;&emsp;&lt;<span class="tag">a</span> href=<span class="string">"#" </span>&gt;click me!&lt;/<span class="tag">a</span>&gt;<br/>
+        <div class="number">11</div>&emsp;&emsp;&emsp;&lt;<span class="tag">p</span>&gt;SIMPing is fun!&lt;/<span class="tag">p</span>&gt;<br/>
+        <div class="number">12</div>&emsp;&emsp;&lt;/<span class="tag">div</span>&gt;<br/>
+        <div class="number">13</div>&emsp;&lt;/<span class="tag">body</span>&gt;<br/>
+        <div class="number">14</div>&lt;/<span class="tag">html</span>&gt;
       </p>
     </div>
   </div>
 </template>
 
 <style>
+.number {
+  font-weight: lighter;
+  width: 32px;
+  float: left;
+}
+
 .string {
-  color:rgb(0, 189, 126);
+  color:rgb(87, 158, 64);
   font-style: italic;
 }
 
 .identifier {
-  color:rgb(0, 189, 126);
+  color:rgb(87, 158, 64);
   font-weight: bold;
 }
 
@@ -57,22 +66,24 @@
 
 .left {
   padding: 8px;
-  width: 45%;
+  width: 356px;
   float: left;
   background-color: white;
   color: black;
-  border: 4px solid rgb(0, 189, 126);
+  border: 4px solid rgb(87, 158, 64);
   margin: 8px;
+  height: 512px;
 }
 
 .right {
   padding: 8px;
-  width: 45%;
+  width: 356px;
   float: right;
   background-color: white;
   color: black;
-  border: 4px solid rgb(0, 189, 126);
+  border: 4px solid rgb(87, 158, 64);
   margin: 8px;
+  height: 512px;
 }
 
 @media (min-width: 1024px) {
